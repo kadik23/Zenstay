d63bc8ecbac426a9ba11bf65c28d09c301214353
unknown <belhadjmohamed380@gmail.com>
2024-01-18 21:22:31 +0100

    Guest Done

--- Frontend/Guest/src/Pages/Profile.jsx
+++ Frontend/Guest/src/Pages/Profile.jsx
@@ -1,14 +1,15 @@
 import ProfileSideBar from "../Components/ProfileSideBar";
+import PersonalDetails from "../Components/PersonalDetails";
 
 export default function Profile() {
     return(
-        <div>
-            <div className="d-flex">
-                <div>
+        <div className="" style={{height:"100vh"}}>
+            <div className="d-flex h-100">
+                <div className="h-100">
                     <ProfileSideBar />
                 </div>
-                <div className="flex-grow-1 px-4" style={{marginLeft:"36opx"}}>
-                    
+                <div className="flex-grow-1 px-4" style={{marginLeft:"36opx",marginTop:"60px"}} >
+                    <PersonalDetails />
                 </div>
             </div>
         </div>
