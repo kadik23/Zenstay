3fea670701e12b5493237aa6ec529751e7fbf72c
unknown <belhadjmohamed380@gmail.com>
2024-01-22 15:22:04 +0100

    Admin Dashboard Page done

---
+++ Frontend/Admin/src/Layouts/RootLayout.jsx
@@ -0,0 +1,20 @@
+import Recent from '../Components/Recent'
+import SideBar from '../Components/SideBar'
+import { Outlet  } from 'react-router-dom'
+export default function RootLayout(){
+
+    return(
+        <div className="d-flex">
+            <div style={{maxWidth:"300px"}}>
+                <SideBar />
+            </div>
+            <div className=" " style={{background:"#fbfbfb"}}>
+                <Outlet />
+            </div>
+            <div className="ps-3 py-4" style={{maxWidth:"280px"}}>
+                <Recent/>
+            </div>
+
+        </div>
+    )
+}
