62ee67b3ce93535b43fb09f929ddcdd6020394c8
kadik23 <kadiksalah03@gmail.com>
2026-08-25 22:16:26 +0100

    quick improve

--- api/index.mjs
+++ api/index.mjs
@@ -24,7 +24,7 @@ import routes from "./routes/index.js"
 const app = express()
 const corsOptions = {
     credentials: true,
-    origin: ['http://localhost:5173', 'https://zenstay-two.vercel.app'],
+    origin: ['http://localhost:5174', 'http://localhost:5173', 'https://zenstay-two.vercel.app'],
 };
 app.use(cors(corsOptions));
 
